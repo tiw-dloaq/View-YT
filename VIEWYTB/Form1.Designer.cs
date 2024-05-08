0d682bfc787b3b1fb3b32850808a9e17e4f4d13e
LA VAN BAO <lavanbao113@gmail.com>
2024-05-08 07:49:48 +0700

    first commit

---
+++ VIEWYTB/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace VIEWYTB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.txtSoLuong = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnStar = new System.Windows.Forms.Button();
+            this.txtSoGiay = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtProxy = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.addProxyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(58, 25);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(463, 22);
+            this.txtURL.TabIndex = 0;
+            // 
+            // txtSoLuong
+            // 
+            this.txtSoLuong.Location = new System.Drawing.Point(436, 81);
+            this.txtSoLuong.Name = "txtSoLuong";
+            this.txtSoLuong.Size = new System.Drawing.Size(85, 22);
+            this.txtSoLuong.TabIndex = 1;
+            this.txtSoLuong.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "URL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(355, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Số Luồng";
+            // 
+            // btnStar
+            // 
+            this.btnStar.Location = new System.Drawing.Point(382, 300);
+            this.btnStar.Name = "btnStar";
+            this.btnStar.Size = new System.Drawing.Size(120, 43);
+            this.btnStar.TabIndex = 4;
+            this.btnStar.Text = "Start";
+            this.btnStar.UseVisualStyleBackColor = true;
+            this.btnStar.Click += new System.EventHandler(this.btnStar_Click);
+            // 
+            // txtSoGiay
+            // 
+            this.txtSoGiay.Location = new System.Drawing.Point(436, 53);
+            this.txtSoGiay.Name = "txtSoGiay";
+            this.txtSoGiay.Size = new System.Drawing.Size(85, 22);
+            this.txtSoGiay.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(309, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Số Giây Muốn Xem";
+            // 
+            // txtProxy
+            // 
+            this.txtProxy.Location = new System.Drawing.Point(21, 99);
+            this.txtProxy.Multiline = true;
+            this.txtProxy.Name = "txtProxy";
+            this.txtProxy.Size = new System.Drawing.Size(258, 244);
+            this.txtProxy.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Proxy";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addProxyToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(211, 56);
+            // 
+            // addProxyToolStripMenuItem
+            // 
+            this.addProxyToolStripMenuItem.Name = "addProxyToolStripMenuItem";
+            this.addProxyToolStripMenuItem.Size = new System.Drawing.Size(210, 24);
+            this.addProxyToolStripMenuItem.Text = "Add Proxy";
+            this.addProxyToolStripMenuItem.Click += new System.EventHandler(this.addProxyToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 362);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtProxy);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSoGiay);
+            this.Controls.Add(this.btnStar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSoLuong);
+            this.Controls.Add(this.txtURL);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.TextBox txtSoLuong;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnStar;
+        private System.Windows.Forms.TextBox txtSoGiay;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtProxy;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem addProxyToolStripMenuItem;
+    }
+}
+
---
+++ VIEWYTB/Form1.cs
@@ -0,0 +1,146 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VIEWYTB
+{
+    public partial class Form1 : Form
+    {
+        ChromeOptions options = new ChromeOptions();
+        ChromeDriverService service = null;
+        public Form1()
+        {
+            InitializeComponent();
+            addProxyToolStripMenuItem.Click += addProxyToolStripMenuItem_Click;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStar_Click(object sender, EventArgs e)
+        {
+            // Tách danh sách proxy từ TextBox vào một mảng
+            string[] proxies = txtProxy.Lines;
+
+            // Lấy số lượng proxy được sử dụng từ TextBox txtSoLuong
+            int soLuongProxy = int.Parse(txtSoLuong.Text);
+
+            // Lấy số lượng tab từ TextBox txtSoGiay
+            int soTab = int.Parse(txtSoGiay.Text);
+
+            // Tạo danh sách các đối tượng ChromeDriver
+            List<ChromeDriver> drivers = new List<ChromeDriver>();
+
+            // Tạo các ChromeDriver mà không cần proxy nếu danh sách proxy rỗng hoặc không đủ số lượng
+            if (proxies.Length == 0 || soLuongProxy > proxies.Length)
+            {
+                // Tạo số lượng ChromeDriver tương ứng với số luồng được chọn
+                for (int i = 0; i < soLuongProxy; i++)
+                {
+                    ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+                    service.HideCommandPromptWindow = true;
+
+                    try
+                    {
+                        ChromeDriver driver = new ChromeDriver(service, options);
+
+                        // Chỉnh kích thước của cửa sổ trình duyệt
+                        driver.Manage().Window.Size = new Size(500, 300);
+                        // Thêm ChromeDriver vào danh sách
+                        drivers.Add(driver);
+                    }
+                    catch (Exception ex)
+                    {
+                        // Xử lý khi không thể tạo ChromeDriver, ví dụ: thông báo lỗi
+                        MessageBox.Show($"Không thể tạo ChromeDriver: {ex.Message}");
+                        return;
+                    }
+
+                    // Đợi một chút trước khi tạo ChromeDriver tiếp theo
+                    Thread.Sleep(1000);
+                }
+            }
+            else // Tạo các ChromeDriver với cấu hình proxy
+            {
+                // Lặp qua mỗi proxy và tạo một ChromeDriver với cấu hình proxy tương ứng
+                for (int i = 0; i < soLuongProxy; i++)
+                {
+                    string proxy = proxies[i];
+
+                    ChromeOptions options = new ChromeOptions();
+
+                    // Thêm cấu hình proxy vào ChromeOptions
+                    options.AddArgument("--proxy-server=" + proxy);
+
+                    ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+                    service.HideCommandPromptWindow = true;
+
+                    try
+                    {
+                        ChromeDriver driver = new ChromeDriver(service, options);
+                        driver.Manage().Window.Size = new Size(500, 300);
+                        // Thêm ChromeDriver vào danh sách
+                        drivers.Add(driver);
+                    }
+                    catch (Exception ex)
+                    {
+                        // Xử lý khi proxy không hoạt động, ví dụ: thông báo lỗi
+                        MessageBox.Show($"Proxy {proxy} không hoạt động: {ex.Message}");
+                    }
+
+                    // Đợi một chút trước khi tạo ChromeDriver tiếp theo
+                    Thread.Sleep(1000);
+                }
+            }
+
+            int thoiGianMiliGiay = soTab * 1000;
+
+            foreach (var driver in drivers)
+            {
+                // Thực hiện các hành động trên mỗi tab
+                driver.Navigate().GoToUrl(txtURL.Text);
+
+                // Thực hiện các hành động khác...
+            }
+
+            // Chờ trong khoảng thời gian đã chỉ định
+            Thread.Sleep(thoiGianMiliGiay);
+
+            // Sau khi đã chờ đủ thời gian, đóng tất cả các tab
+            foreach (var driver in drivers)
+            {
+                // Tắt tab
+                driver.Quit();
+            }
+        }
+
+        private void addProxyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Mở hộp thoại chọn tệp
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.Multiselect = false;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Đọc nội dung của tệp đã chọn và thêm vào TextBox "txtProxy"
+                string filePath = openFileDialog.FileName;
+                string[] lines = File.ReadAllLines(filePath);
+                txtProxy.Lines = lines;
+            }
+        }
+    }
+}
